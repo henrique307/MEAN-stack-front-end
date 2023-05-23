88711f1fda4c7eb09f760491780157c0690d52a4
henrique <henrique.florencio307@gmail.com>
2023-05-22 21:11:51 -0300

    modificações para compartilhar o front-end

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Server apenas o arquivo stático do repo dist
+app.use(express.static('./dist/app'));
+
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', {root: 'dist/angular-heroku'})
+})
+
+// Começa a escutar na porta padrão do heroku
+app.listen(process.env.PORT || 8080);
